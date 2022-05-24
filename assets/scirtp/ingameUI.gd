b23b2512ae0ffb0efa378962931280b41e920fc9
Utfigyii <42611989+Utfigyii@users.noreply.github.com>
2022-05-24 11:27:42 +0300

    fix menu and winconditions

--- assets/scirtp/ingameUI.gd
+++ assets/scirtp/ingameUI.gd
@@ -3,6 +3,7 @@ extends Control
 var score1 = 0
 var score2 = 0
 var ignoreScore = false
+export var targetScore = 10
 signal restart()
 
 var menuscene = load("res://menu.tscn")
@@ -14,7 +15,7 @@ func _on_gameLoader_updateScore():
 	$"1goals".value = score1
 	$"2goals".value = score2
 	print("scoreUpdated")
-	if (score1 == 1 or score2 == 1) and !ignoreScore:
+	if (score1 == targetScore or score2 == targetScore) and !ignoreScore:
 		$WindowDialog.popup()
 		$AudioStreamPlayer.play()
 
