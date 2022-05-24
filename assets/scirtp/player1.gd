00aaeaae746d2c86d1965d915c167d0ae1a8a1a4
Utfigyii <42611989+Utfigyii@users.noreply.github.com>
2022-05-24 11:43:51 +0300

    add android buttons

--- assets/scirtp/player1.gd
+++ assets/scirtp/player1.gd
@@ -12,3 +12,16 @@ func _physics_process(delta):
 	if Input.is_action_just_released("a"):
 		$AnimationPlayer.play("legback")
 		$bodybody/kickfoot/CollisionShape.disabled = true
+
+
+func _on_plaer1Button_button_down():
+	$AnimationPlayer.play("kick")
+	$bodybody/kickfoot/CollisionShape.disabled = false
+	if $bodybody/groundCheck.is_colliding():
+		$bodybody.apply_impulse(Vector3(0, 0, 0), Vector3($bodybody.rotation_degrees.z * -0.3, 30, 0))
+		print($bodybody.rotation_degrees)
+		$bodybody.apply_torque_impulse(Vector3(0, 0, 20))
+
+func _on_plaer1Button_button_up():
+	$AnimationPlayer.play("legback")
+	$bodybody/kickfoot/CollisionShape.disabled = true
--- assets/scirtp/player4.gd
+++ assets/scirtp/player4.gd
@@ -11,3 +11,15 @@ func _physics_process(delta):
 	if Input.is_action_just_released("l"):
 		$AnimationPlayer.play("legback")
 		$bodybody/kickfoot/CollisionShape.disabled = true
+
+
+func _on_plaer4Button_button_down():
+	$AnimationPlayer.play("kick")
+	$bodybody/kickfoot/CollisionShape.disabled = false
+	if $bodybody/groundCheck.is_colliding():
+		$bodybody.apply_impulse(Vector3(0, 0, 0), Vector3($bodybody.rotation_degrees.z * 0.3, 30, 0))
+		$bodybody.apply_torque_impulse(Vector3(0, 0, -20))
+
+func _on_plaer4Button_button_up():
+	$AnimationPlayer.play("legback")
+	$bodybody/kickfoot/CollisionShape.disabled = true
