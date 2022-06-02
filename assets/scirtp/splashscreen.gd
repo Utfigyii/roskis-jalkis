ebe859a64d7925573f17945aa61f2861fd33896e
Utfigyii <42611989+Utfigyii@users.noreply.github.com>
2022-06-02 17:15:02 +0300

    add warning ja build

--- assets/scirtp/splashscreen.gd
+++ assets/scirtp/splashscreen.gd
@@ -5,3 +5,9 @@ var gamingscene = preload("res://menu.tscn")
 func _on_AnimationPlayer_animation_finished(anim_name):
 	get_tree().change_scene_to(gamingscene)
 
+
+
+func _on_Button_pressed():
+	$LoudWarnin.visible = false
+	$AudioStreamPlayer.play()
+	$AnimationPlayer.play("intro spalsh")
